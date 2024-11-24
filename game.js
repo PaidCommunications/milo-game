43f706585397128c21845e68b2cbd545619cb45b
PaidCommunications <julian@paidcommunications.com>
2024-11-23 19:29:28 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,29 +1,73 @@
-// Only need to modify the shooting function in your code:
-
-    // Modified shooting function with 7 bullets
+// Replace only the shoot() function in your working code:
+    
     function shoot() {
         if (player.spreadShot) {
-            // Seven bullet spread
-            const bulletPositions = [
-                { x: -18, y: 0 },  // Far left
-                { x: -12, y: 0 },  // Mid left
-                { x: -6, y: 0 },   // Near left
-                { x: 0, y: 0 },    // Center
-                { x: 6, y: 0 },    // Near right
-                { x: 12, y: 0 },   // Mid right
-                { x: 18, y: 0 }    // Far right
-            ];
+            // Left bullets
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 5, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
             
-            bulletPositions.forEach((pos) => {
-                add([
-                    rect(6, 15),
-                    pos(player.pos.x + 22 + pos.x, player.pos.y),
-                    color(255, 255, 0),
-                    move(UP, 400),
-                    area(),
-                    "bullet"
-                ]);
-            });
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 15, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 25, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
+            // Center bullet
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 22, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
+            // Right bullets
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 35, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 45, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
+            add([
+                rect(6, 15),
+                pos(player.pos.x + 55, player.pos.y),
+                color(255, 255, 0),
+                move(UP, 400),
+                area(),
+                "bullet"
+            ]);
+            
         } else {
             // Normal single shot
             add([
